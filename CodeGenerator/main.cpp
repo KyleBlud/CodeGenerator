082cf385b06a214a17d3a62bf1336b032d4ed95e
KyleBlud <kylebludworth@outlook.com>
2016-04-11 13:04:12 -0400

    Initial Commit

---
+++ CodeGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CodeGenerator
+//
+//  Created by Kyle Bludworth on 4/11/16.
+//  Copyright © 2016 Kyle Bludworth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
